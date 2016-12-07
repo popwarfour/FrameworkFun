8a3a776879f8aaa3bb34dcaac23a9ccd70e3f50f
Anders Melen <popwarfour2@gmail.com>
2016-12-06 23:53:40 -0500

    setup project

---
+++ FrameworkFun/FrameworkFun/WundergroundLocation+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  WundergroundLocation+CoreDataClass.swift
+//  FrameworkFun
+//
+//  Created by Anders Melen on 12/6/16.
+//  Copyright © 2016 Anders Melen. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+//public class WundergroundLocation: NSManagedObject {
+//
+//}
---
+++ FrameworkFun/Pods/Target Support Files/Pods-FrameworkFun/Pods-FrameworkFun-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FrameworkFun : NSObject
+@end
+@implementation PodsDummy_Pods_FrameworkFun
+@end
